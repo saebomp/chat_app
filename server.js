970c5804302933ff310d880dc8c7d25435c739c2
SAEBOMPAK <saebompak@gmail.com>
2021-09-20 03:28:04 +0900

    add connect

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
+// Run when client connects
+io.on('connection', socket => {
+
+    //Welcome current user
+    socket.emit('message', 'Welcome to ChatCord'); 
+    //for single clients
+    
+    //Broadcast when a user connetcs
+    socket.broadcast.emit('message', 'A user has joined the chat'); 
+    //for all of the clients except the client that's connecting
+
+    //Runs when client disconnects
+    socket.on('disconnect', ()=> {
+        io.emit('message', 'A user has left the chat') 
+    //all the clients
+    })
+    
+})
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
 //npm run dev
-// https://www.youtube.com/watch?v=jD7FnbI76Hg   8:47
+// https://www.youtube.com/watch?v=jD7FnbI76Hg   17:20
